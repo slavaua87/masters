684f3f803ec1d10e9346f1196d2a32f12853582c
sn <slavaua87@gmail.com>
2015-04-26 13:23:42 -0400

    new sampler
    Former-commit-id: 676ac432bcf29fbc36b655dcdfbc7317f578cfa2

--- src/predictions/behavior/test_boot.R
+++ src/predictions/behavior/test_boot.R
@@ -8,7 +8,7 @@ library("boot")
 source("src/fitting/wiener_parameters.R")
 source("src/fitting/combine_parameters.R")
 
-n <- 1e4
+n <- 1e5
 m <- 1e3
 k <- 50
 ind_param <- combine_param(nu, wiener, rho, omega)
@@ -25,11 +25,13 @@ stat_boot <- function(data, indices) {
   data_boot <- slice(data, indices)
   rt <- dplyr::select(data_boot, q) * 1000
   p <- dplyr::select(data_boot, resp) %>% unlist %>% as.numeric %>% subtract(1)
-  stat <- c(quantile(rt$q, probs = .9, type = 8), 1 - mean(p))
+  stat <- c(quantile(rt$q, probs = .1, type = 8), 1 - mean(p))
   return(stat)
 }
-
+timer <- proc.time()
 dat <- dat_sim(pars, n, m, k)
-res <- boot(dat, stat_boot, 1000, parallel = "multicore", ncpus = 50)
-boot.ci(res, type = c("basic", "norm", "perc"), index = 1)
-boot.ci(res, type = c("basic", "norm", "perc"), index = 2)
+timer <- proc.time() - timer
+
+#res <- boot(dat, stat_boot, 100, parallel = "multicore", ncpus = 10)
+#boot.ci(res, type = c("basic", "norm", "perc"), index = 1)
+#boot.ci(res, type = c("basic", "norm", "perc"), index = 2)
